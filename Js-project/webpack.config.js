4d9f977c3aa0f366f46d08f5372b45b1fc3f6fe1
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-31 20:31:26 -0700

    Add gitignore and get webpack up

--- Js-project/webpack.config.js
+++ Js-project/webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path");
 module.exports = {
   context: __dirname,
-  entry: "./js/main.js",
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname),
     filename: "./bundle.js"
