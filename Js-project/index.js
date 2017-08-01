aef141f9a67bcda0a7ebe3b58e51dde626d73b6c
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-07-31 22:02:22 -0700

    Get basic understanding of canvas drawing, make cool 80s art

--- Js-project/index.js
+++ Js-project/index.js
@@ -1,5 +1,40 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'lightblue';
-ctx.fillRect(10, 10, 100, 100);
+console.log("hey there");
+
+var canvas = document.getElementById('canvas');
+
+console.log(canvas);
+var c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+c.fillStyle = 'rgba(0, 0, 255, 0.5)'
+c.fillRect(0, 0, 100, 100);
+
+
+c.beginPath();
+c.moveTo(150, 150);
+c.lineTo(175, 165);
+c.lineTo(100, 185);
+c.strokeStyle = "#fa34a3";
+c.stroke();
+
+
+
+
+for (let i = 0; i < 5; i++) {
+  c.beginPath();
+  c.arc(300 + i*3, 300 - i*3, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = "#0034a3";
+  c.stroke();
+}
+
+
+
+
+// var canvas = document.getElementById('canvas');
+// var ctx = canvas.getContext('2d');
+//
+// ctx.fillStyle = 'lightblue';
+// ctx.fillRect(10, 10, 100, 100);
---
+++ Js-project/lib/board.js
@@ -0,0 +1,10 @@
+
+
+
+//
+//
+// document.addEventListener("DOMContentLoaded")
+//
+// function(){
+//   const canvas = document.getElementById('canvas');
+// }
