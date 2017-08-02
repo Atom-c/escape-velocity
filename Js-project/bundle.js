6c4614b18d3e947a80cfc6e673a01c4b1ddfdb80
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-08-02 14:42:17 -0700

    Begin work on giant conditionals for obstacle handling

--- Js-project/bundle.js
+++ Js-project/bundle.js
@@ -70,344 +70,75 @@
 "use strict";
 
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+(function () {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
 
-var _avatar = __webpack_require__(1);
+window.onload = function init() {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  gameLoop();
+};
 
-var _avatar2 = _interopRequireDefault(_avatar);
+window.addEventListener("keydown", function (e) {
+  keys[e.keyCode] = true;
+});
+window.addEventListener("keyup", function (e) {
+  keys[e.keyCode] = false;
+});
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var avatarX = 400,
+    avatarY = 300,
+    velX = 0,
+    velY = 0,
+    keys = [],
+    maxSpeed = 10,
+    firstBlockLeft = 0,
+    firstBlockWidth = 100,
+    secondBlockLeft = 300,
+    secondBlockWidth = 200,
+    avatarXleft = 25,
+    avatarXright = 25,
+    firstBlockBottom = 0,
+    secondBlockBottom = 0,
+    firstBlockHeight = 20,
+    secondBlockHeight = 20;
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function gameLoop() {
+  whatKey();
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 600;
 
-var canvas = document.getElementById('canvas');
-var c = canvas.getContext('2d');
-function init() {
-  setInterval(Riser.draw, Math.ceil(1000 / 60));
-  canvas.width = 700;
-  canvas.height = 500;
-  riser = new Riser(canvas.width / 2, canvas.height / 2, 1, 1, 10);
-  riser.update();
-}
+  avatarX += velX;
+  avatarY += velY;
 
-//riser
-
-var Riser = function () {
-  function Riser(x, y, dx, dy, radius) {
-    _classCallCheck(this, Riser);
-
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
+  if (avatarY < 0) {
+    avatarY = 0;
+    velY = 0;
+  } else if (avatarY < 0) {
+    avatarY = 0;
+    velY = 0;
+  } else if (avatarX + avatarXleft < secondBlockLeft && avatarY > secondBlockHeight) {
+    velY -= 0.25;
   }
 
-  _createClass(Riser, [{
-    key: 'update',
-    value: function update() {
-      if (this.y + this.radius / 1000 < 0) {
-        this.y = 0;
-        this.dy = 0;
-      } else {
-        this.dy += 0.25;
-      }
-      this.y -= this.dy;
-      this.draw();
-    }
-  }, {
-    key: 'slide',
-    value: function slide(dir) {
-      if (this.x + this.radius / 1000 < 0) {
-        this.x = 0;
-        this.dx = 1;
-      } else if (this.x + this.radius / 1000 > canvas.width) {
-        this.x = canvas.width;
-        this.dx = 1;
-      } else {
-        if (dir === 'LEFT') {
-          this.dx += 1.25;
-          this.x -= this.dx;
-          this.draw();
-        } else {
-          this.dx -= 1.25;
-          this.x -= this.dx;
-          this.draw();
-        }
-      }
-    }
-  }, {
-    key: 'draw',
-    value: function draw() {
-      c.clearRect(0, 0, canvas.width, canvas.height);
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = 'red';
-      c.fill();
-      c.closePath();
-    }
-  }, {
-    key: 'onKeyDown',
-    value: function onKeyDown(event) {
-
-      switch (event.keyCode) {
-        case 37:
-          // left
-          console.log("YEAH PRESSED LEFT!");
-          console.log(riser);
-          // riser.update();
-          riser.x -= Math.floor(canvas.width * 0.055);
-
-          break;
-        case 65:
-          // A (left)
-          console.log("YEAH PRESSED THE A KEY!");
-          if (riser.x - riser.dx > 0) {
-            riser.x -= riser.dx * 25;
-            riser.draw();
-          }
-          break;
-        case 39:
-          // right
-          console.log("YEAH PRESSED RIGHT!");
-          console.log(riser);
-          if (riser.x + riser.dx < canvas.width) {
-            riser.x += Math.floor(canvas.width * 0.08);
-            // riser.slide("RIGHT");
-          }
-          break;
-        case 68:
-          // D (right)
-          console.log("YEAH PRESSED THE D KEY!");
-          if (riser.x + riser.dx < canvas.width) {
-            riser.x += riser.dx * 25;
-            riser.draw();
-          }
-          break;
-      }
-    }
-  }]);
-
-  return Riser;
-}();
-//riser
-
-// instantiate riser and init everything
-
-
-var riser;
-// function init() {
-
-// riser.update()
-
-// }
-
-
-function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, canvas.width, canvas.height);
-  riser.update();
+  var avatar = ctx.fillRect(avatarX, avatarY, avatarXleft, avatarXright);
+  var firstBlock = ctx.fillRect(firstBlockLeft, firstBlockBottom, firstBlockWidth, firstBlockHeight);
+  var secondBlock = ctx.fillRect(secondBlockLeft, secondBlockBottom, secondBlockWidth, secondBlockHeight);
+  requestAnimationFrame(gameLoop);
 }
 
-init();
-window.addEventListener('keydown', riser.onKeyDown);
-animate();
-
-//
-//
-// function pageRender () {
-//   slideAvatar();
-//   riseAvatar();
-// }
-//
-//
-// function drawStuff() {
-//   ref_int setTimeout(pageRender, Math.ceil(1000/60))
-// }
-//
-// function reset() {
-//   clearTimeout(ref_int);
-//   //reset the stuff to old spot
-// }
-//ref_int = requestAnimationFrame(pageRender)
-//
-//cancelAnimationFrame(ref_int)
-//
-
-
-// var mouse = {
-//   x: undefined,
-//   y: undefined,
-// }
-//
-// var minRadius = 10;
-// var maxRadius = 40;
-//
-// var colorArray = [
-//   `rgba(39, 146, 204, 0.3)`,
-//   `rgba(75, 126, 153, 0.5)`,
-//   `rgba(23, 255, 211, 0.7)`,
-//   `rgba(255, 108, 204, 0.3)`,
-//   `rgba(255, 108, 87, 0.5)`,
-// ];
-//
-// canvas.addEventListener('mousemove',
-//   function (event) {
-//     // console.log(event);
-//     mouse.x = event.x;
-//     mouse.y = event.y;
-// })
-//
-// canvas.addEventListener('resize', function () {
-//   canvas.width = window.innerWidth;
-//   canvas.height = window.innerHeight;
-//
-//   init();
-// })
-//
-// canvas.addEventListener('keypress', function (event) {
-//   var keyCode = event.keyCode;
-//   if (keyCode == 37) {
-//     // new Circle(1, 1, 1, 1, 60));
-//     c.draw
-//   }
-// })
-//
-//
-//
-// function Circle(x, y, dx, dy, radius) {
-//   this.x = x;
-//   this.y = y;
-//   this.dx = dx;
-//   this.dy = dy;
-//   this.radius = radius;
-//   this.minRadius = minRadius;
-//   this.maxRadius = maxRadius;
-//   this.piTime = (Math.random() * 2)
-//   this.color = colorArray[Math.floor(Math.random() * colorArray.length)]
-//
-//   this.draw = function() {
-//     c.beginPath();
-//     c.arc(this.x, this.y, this.radius, 0, Math.PI * this.piTime, false);
-//     c.strokeStyle = this.color;
-//     c.stroke();
-//     c.fillStyle = this.color;
-//     c.fill();
-//   }
-//
-//   this.update = function () {
-//     if (this.x + this.radius > innerWidth
-//     || this.x - this.radius < 0) {
-//       this.dx = -this.dx;
-//     }
-//
-//     if (this.y + this.radius > innerWidth
-//     || this.y - this.radius < 0) {
-//       this.dy = -this.dy;
-//     }
-//
-//     this.x += this.dx;
-//     this.y += this.dy;
-//
-//     if (mouse.x - this.x < 50 && mouse.x - this.x > -50
-//     && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-//       if (this.radius < this.maxRadius) {
-//         this.radius += 1;
-//
-//       }
-//     } else if (this.radius > this.minRadius){
-//       this.radius -= 1;
-//     }
-//
-//     this.draw();
-//   }
-// }
-//
-//
-// var circleArray = [];
-//
-// function init() {
-//
-//   circleArray = [];
-//
-//   for (var i = 0; i < 2000; i++) {
-//     var radius = Math.random() * 3 + 1;
-//     var x = Math.random() * (innerWidth - radius * 2) + radius;
-//     var y = Math.random() * (innerHeight - radius * 2) + radius;
-//     var dx = (Math.random() - 0.5);
-//     var dy = (Math.random() - 0.5);
-//     circleArray.push(new Circle(x, y, dx, dy, radius));
-//   }
-// }
-//
-// var circle = new Circle(200, 200, 3, 3, 30);
-//
-// function animate() {
-//   requestAnimationFrame(animate);
-//
-//   c.clearRect(0,0,innerWidth,innerHeight);
-//
-//   for (var i = 0; i < circleArray.length; i++) {
-//     circleArray[i].update();
-//   }
-//
-// }
-//
-// init();
-// animate();
-
-
-// c.beginPath();
-// c.arc(x, y, 30, 0, Math.PI * 2, false);
-// c.strokeStyle = `#000`;
-// c.stroke();
-
-// var canvas = document.getElementById('canvas');
-// var ctx = canvas.getContext('2d');
-//
-// ctx.fillStyle = 'lightblue';
-// ctx.fillRect(10, 10, 100, 100);
-
-
-// c.fillStyle = 'rgba(0, 0, 255, 0.5)'
-// c.fillRect(0, 0, 100, 100);
-//
-//
-// c.beginPath();
-// c.moveTo(150, 150);
-// c.lineTo(175, 165);
-// c.lineTo(100, 185);
-// c.strokeStyle = "#fa34a3";
-// c.stroke();
-
-
-// for (let i = 0; i < 35; i++) {
-//   c.beginPath();
-//   c.arc(Math.random() * window.innerWidth, Math.random() * window.innerHeight, 30, 0, Math.PI * 2, false);
-//   c.strokeStyle = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-//   c.fillStyle = 'rgba(0, 0, 255, 0.5)'
-//   c.stroke();
-// }
-
-
-// for (let i = 0; i < 35; i++) {
-// var x = Math.random() * window.innerWidth;
-// var y = Math.random() * window.innerHeight;
-// var dx = (Math.random() - 0.5) * 10;
-// var dy = (Math.random() - 0.5) * 10;
-// var radius = 30;
-
-// var x = Math.random() * window.innerWidth;
-// var y = Math.random() * window.innerHeight;
-// var dx = (Math.random() - 0.5) * 10;
-// var dy = (Math.random() - 0.5) * 10;
-// var radius = 30;
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-throw new Error("Module build failed: SyntaxError: Unexpected token (1:9)\n\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 1 | \u001b[39m\u001b[36mfunction\u001b[39m () {\n \u001b[90m   | \u001b[39m         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 2 | \u001b[39m  \u001b[36mvar\u001b[39m requestAnimationFrame \u001b[33m=\u001b[39m window\u001b[33m.\u001b[39mrequestAnimationFrame\n \u001b[90m 3 | \u001b[39m}()\u001b[33m;\u001b[39m\n \u001b[90m 4 | \u001b[39m\u001b[0m\n");
+function whatKey() {
+  if (keys[37]) {
+    avatarX -= 20;
+  }
+  if (keys[39]) {
+    avatarX += 20;
+  }
+}
 
 /***/ })
 /******/ ]);
--- Js-project/lib/avatar.js
+++ Js-project/lib/avatar.js
@@ -5,6 +5,8 @@ function () {
 window.onload = function init() {
   var canvas = documentgetElementById("canvas");
   var c = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 600;
   caveLoop();
 }
 
@@ -21,6 +23,7 @@ class Avatar {
     this.x = 350 //Board.width / 2 later
     this.y = 450
     this.dx = 0
+    this.radius = 20;
     keys = []
   }
 
@@ -29,12 +32,24 @@ class Avatar {
 
 var avatar = Avatar.new();
 
+draw () {
+  c.clearRect(0, 0, canvas.width, canvas.height)
+  c.beginPath();
+  c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+  c.fillStyle = 'blue'
+  c.fill();
+  c.closePath();
+}
+
 function caveLoop() {
   whatKey();
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
   canvas.width = 800;
   canvas.height = 600;
+
+  draw();
+  requestAnimationFrame(caveLoop);
 }
 
 function whatKey() {
