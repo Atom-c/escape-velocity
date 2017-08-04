d9ec4ed1bb885702c3311bed01848bc898db88cb
%= GitConfig.user_name % <atom.colour@gmail.com>
2017-08-04 12:06:36 -0700

    Final push

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function gameLoop() {
       arr.push(new Obstacle(bX, 0, 280, 3, (8), ctx))
       arr.push(new Obstacle(bX * (counter % 33), 0, 330, 3, (8), ctx))
       arr.push(new Obstacle(bX * (counter % 14), 0, 180, 3, (8), ctx))
-
+      console.log(arr.length);
     } else if (counter * 1.5 > 10000) {
       arr.push(new Obstacle(bX, 0, 280, 3, (10), ctx))
       arr.push(new Obstacle(bX * (counter % 33), 0, 330, 3, (10), ctx))
